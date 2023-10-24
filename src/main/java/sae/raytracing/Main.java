1c0af950d128ba992daf8264f31cae514a3b8393
nicolas_lapotre <nicolas_lapotre@ens.univ-artois.fr>
2023-10-24 12:44:55 +0200

    close #7

--- src/main/java/sae/raytracing/Main.java
+++ src/main/java/sae/raytracing/Main.java
@@ -1,8 +1,18 @@
 package sae.raytracing;
 
+
+import sae.raytracing.model.Parser;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        file = "";
-        Parser.reader(file);
+        File textFile = new File(System.getProperty("user.dir") + "/" + args[0]);
+        System.out.println(textFile.getName());
+        if (textFile.exists()) {
+            Parser.reader(textFile);
+        } else {
+            System.err.println("File : " + args[0] + " not found !");
+        }
     }
 }
