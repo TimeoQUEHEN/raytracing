448844aa2fd6ab6e2e5fe3fc068e62fa31cf0387
Thomas Mouton <thomas_mouton2@ens.univ-artois.fr>
2023-10-24 12:55:29 +0200

    modification of the parser

--- src/main/java/sae/raytracing/model/Parser.java
+++ src/main/java/sae/raytracing/model/Parser.java
@@ -15,6 +15,29 @@ public class Parser {
         return false;
     }
 
+    public String[] elementGeoInformation(String nameFile){
+        try {
+            File file=new File(nameFile);
+            Scanner sc=new Scanner(file);
+            while(sc.hasNextLine())
+            {
+                String line = sc.nextLine();
+                String[] tmp = line.split(" ");
+                if(skipComment(line) && (tmp[0].equals("tri") || tmp[0].equals("sphere"))){
+                    String[] res = new String[tmp.length-1];
+                    for (int i = 1; i < tmp.length; i++) {
+                        res[i-1] = tmp[i];
+                    }
+                    return res;
+                }
+            }
+            sc.close();
+        }catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+        return new String[0];
+    }
+
     public String[] keyWordInformation(String word, String nameFile){
         try {
             File file=new File(nameFile);
@@ -40,23 +63,36 @@ public class Parser {
     }
 
 
-    public void reader(String nameFile) {
-        String geo;
-        String[] size = keyWordInformation("size", nameFile);
-        String[] output = keyWordInformation("output", nameFile);
-        String[] camera = keyWordInformation("camera", nameFile);
-        String[] ambient = keyWordInformation("ambient", nameFile);
-        String[] geometry = keyWordInformation("", nameFile);
-        switch (geometry[0]){
-            case "tri":
-                System.out.println("triangle");
-                break;
-            case "sphere":
-                System.out.println("sphere");
-                break;
-            default:
-                System.out.println("erreur");
-                break;
+    public void reader(String nameFile){
+        try {
+            File file=new File(nameFile);
+            Scanner sc=new Scanner(file);
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.charAt(0) != '#') {
+                    String[] keyWord = line.split(" ");
+                }
+            }
+            String geo;
+            String[] size = keyWordInformation("size", nameFile);
+            String[] output = keyWordInformation("output", nameFile);
+            String[] camera = keyWordInformation("camera", nameFile);
+            String[] ambient = keyWordInformation("ambient", nameFile);
+            String[] geometry = elementGeoInformation(nameFile);
+            switch (geometry[0]){
+                case "tri":
+                    System.out.println("triangle");
+                    break;
+                case "sphere":
+                    System.out.println("sphere");
+                    break;
+                default:
+                    System.out.println("erreur");
+                    break;
+            }
+            sc.close();
+        }catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
         }
     }
 }
