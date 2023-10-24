23812751b105168489180701758a28e856ebc1da
Thomas Mouton <thomas_mouton2@ens.univ-artois.fr>
2023-10-24 09:47:36 +0200

    fix reader function

--- src/main/java/sae/lanceurderayons/model/Parser.java
+++ src/main/java/sae/lanceurderayons/model/Parser.java
@@ -1,26 +1,26 @@
 package sae.lanceurderayons.model;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Parser {
-    public void reader(String file) {
+    public void reader(String nameFile) {
         try {
-            String line;
-            String data;
-            File myObj = new File("/export/etu/thomas.mouton/IdeaProjects/test/src/sae/lanceurderayons/model/filename.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                if((line = myReader.nextLine()).startsWith("#")) {
-                    myReader.nextLine();
+            File file=new File(nameFile);
+            Scanner sc=new Scanner(file);
+            while(sc.hasNextLine())
+            {
+                String line = sc.nextLine();
+                if(line.charAt(0) != '#'){
+                    System.out.println(line);
                 }
-                data = myReader.nextLine();
-                System.out.println(data);
             }
-            myReader.close();
+            sc.close();
         }catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
         }
     }
+
 }
