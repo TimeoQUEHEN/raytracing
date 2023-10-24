39f6082063d8c573f1f1a0477d935fc926435967
Jules Langagne <jules_langagne@ens.univ-artois.fr>
2023-10-24 11:35:28 +0200

    Implement methods from IBuilder to SceneBuilder

--- src/main/java/sae/lanceurderayons/model/SceneBuilder.java
+++ src/main/java/sae/lanceurderayons/model/SceneBuilder.java
@@ -1,5 +1,33 @@
 package sae.lanceurderayons.model;
 
-public class SceneBuilder{
+public class SceneBuilder implements IBuilder{
+    private int height, width;
+    private Camera camera;
+    private ILight[] lights;
+    private IElements[] elements;
+    @Override
+    public void setDimensions(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
 
+    @Override
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+    }
+
+    @Override
+    public void addLights(ILight[] lights) {
+        this.lights = lights;
+    }
+
+    @Override
+    public void addElements(IElements[] elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    public Scene Scene() {
+        return new Scene(this.height,this.width,this.camera,this.lights,this.elements);
+    }
 }
