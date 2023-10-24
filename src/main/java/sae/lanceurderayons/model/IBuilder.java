e649025b147db78b407c8adb43167d3a4e95ccc3
Jules Langagne <jules_langagne@ens.univ-artois.fr>
2023-10-24 11:32:49 +0200

    Creation of methods of IBuilder

--- src/main/java/sae/lanceurderayons/model/IBuilder.java
+++ src/main/java/sae/lanceurderayons/model/IBuilder.java
@@ -1,5 +1,9 @@
 package sae.lanceurderayons.model;
 
 public interface IBuilder {
-
+    void setDimensions(int height, int width);
+    void setCamera(Camera camera);
+    void addLights(ILight[] lights);
+    void addElements(IElements[] elements);
+    Scene Scene();
 }
