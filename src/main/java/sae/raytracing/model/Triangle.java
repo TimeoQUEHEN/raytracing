9cf317eab4a7a4cbd4f19c2913249809f2250d4e
Jules Langagne <jules_langagne@ens.univ-artois.fr>
2023-10-25 16:53:44 +0200

    Implement method Triangle.getIntersection

--- src/main/java/sae/raytracing/model/Triangle.java
+++ src/main/java/sae/raytracing/model/Triangle.java
@@ -36,7 +36,17 @@ public class Triangle implements IElements {
 
     @Override
     public double getIntersection(Vector d, Camera cam) {
-        return 0;
+        Point eye = cam.getLookFrom();
+        Point a = this.pX;
+        Vector n = new Vector((this.pY.substraction(a.getCoords())).vectorProduct(this.pZ.substraction(a.getCoords())).norm());
+        double t = a.substraction(eye.getCoords()).scalarProduct(n.getDestDirNorm())/d.scalarProduct(n.getDestDirNorm());
+        Triplet p = eye.getCoords().addition(d.multiplyUsingAScalar(t));
+
+        if (n.scalarProduct((this.pY.getCoords().substraction(a.getCoords())).vectorProduct(p.substraction(a.getCoords()))) >= 0 &&
+                n.scalarProduct((this.pZ.getCoords().substraction(this.pY.getCoords())).vectorProduct(p.substraction(this.pY.getCoords()))) >= 0 &&
+                n.scalarProduct((a.getCoords().substraction(this.pZ.getCoords())).vectorProduct(p.substraction(this.pZ.getCoords()))) >= 0){
+            return t;
+        } else {return -1;}
     }
 
     @Override
