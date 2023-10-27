ace30c384fc7cae98ccfd17bed052a29455b19c0
nicolas_lapotre <nicolas_lapotre@ens.univ-artois.fr>
2023-10-27 16:26:07 +0200

    Modification de la méthode model de l'interface IStrategy

--- src/main/java/sae/raytracing/model/IStrategy.java
+++ src/main/java/sae/raytracing/model/IStrategy.java
@@ -2,5 +2,5 @@ package sae.raytracing.model;
 
 public interface IStrategy {
 
-    public Color model(Scene scene, IElements element, Point p);
+    public Color model(Scene scene, IElements element, Point p, Vector d, ILight light);
 }
