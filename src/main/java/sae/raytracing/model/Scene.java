d820e30ac2e631f060e93387521f34caa1253779
nicolas_lapotre <nicolas_lapotre@ens.univ-artois.fr>
2023-10-27 16:30:29 +0200

    Ajout de l'attribut shadow et de son getter dans la classe Scene

--- src/main/java/sae/raytracing/model/Scene.java
+++ src/main/java/sae/raytracing/model/Scene.java
@@ -3,6 +3,7 @@ package sae.raytracing.model;
 import java.util.ArrayList;
 
 public class Scene {
+    private final boolean shadow;
     private int height;
     private int width;
     private Camera camera;
@@ -10,11 +11,12 @@ public class Scene {
     private ArrayList<ILight> lights;
     private ArrayList<IElements> elements;
 
-    public Scene(int width, int height, Camera camera, Color ambient, ArrayList<ILight> lights, ArrayList<IElements> elements) {
+    public Scene(int width, int height, Camera camera, Color ambient, boolean shadow, ArrayList<ILight> lights, ArrayList<IElements> elements) {
         this.height = height;
         this.width = width;
         this.camera = camera;
         this.ambient = ambient;
+        this.shadow = shadow;
         this.lights = lights;
         this.elements = elements;
     }
@@ -32,6 +34,8 @@ public class Scene {
     }
 
     public Color getAmbient() { return ambient; }
+
+    public boolean getShadow() { return shadow; }
 
     public ArrayList<ILight> getLights() {
         return lights;
