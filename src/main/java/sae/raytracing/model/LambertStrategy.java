809255ca3b5e78b46ca4bdf55351e1644ae04d64
timeo_quehen <timeo_quehen@ens.univ-artois.fr>
2023-10-29 18:36:35 +0100

    Addition of DOcstring in LambertStrategy

--- src/main/java/sae/raytracing/model/LambertStrategy.java
+++ src/main/java/sae/raytracing/model/LambertStrategy.java
@@ -4,6 +4,17 @@ public class LambertStrategy implements IStrategy{
 
     private IStrategy childStrat;
 
+    /**
+     * Calculate the color of a pixel in the scene using the Lambert model.
+     * this model is used when the keyword diffuse is used in the input file
+     *
+     * @param  scene    the scene in which the model is rendered
+     * @param  element  the element being rendered
+     * @param  p        the coordinates of the pixel
+     * @param  d        the direction of the pixel
+     * @param  light    the light source in the scene
+     * @return          the color of the pixel
+     */
     @Override
     public Color model(Scene scene, IElements element, Point p, Vector d, ILight light) {
         childStrat = new BaseStrategy();
