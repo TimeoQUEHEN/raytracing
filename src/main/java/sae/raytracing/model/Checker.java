ccd6f2ea3426d5ce814459b288d91343f1de8654
timeo_quehen <timeo_quehen@ens.univ-artois.fr>
2023-10-29 15:20:10 +0100

    Implementing docstring for the class Checker

--- src/main/java/sae/raytracing/model/Checker.java
+++ src/main/java/sae/raytracing/model/Checker.java
@@ -1,6 +1,23 @@
 package sae.raytracing.model;
 
 public class Checker implements IStrategy{
+
+    /**
+     * Calculates the color of a point in a plane using a checkerboard pattern.
+     * Gathers the coordinates x and z, and normalises them.
+     * if both are inferior to 0.5, the color is scene.getCheckerC1()
+     * or, if both are superior to 0.5, the color is scene.getCheckerC1()
+     * Otherwise, the color is scene.getCheckerC2()
+     *
+     * the parameters d and light are not used, but need to be implemented due to the development pattern used.
+     *
+     * @param  scene    the scene being rendered
+     * @param  element  the elements in the scene
+     * @param  p        the point in the scene
+     * @param  d        the direction vector
+     * @param  light    the light source
+     * @return          the color of the point
+     */
     @Override
     public Color model(Scene scene, IElements element, Point p, Vector d, ILight light) {
         double x = p.getX() %(2*scene.getCheckerSize());
